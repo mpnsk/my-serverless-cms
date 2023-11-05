b50380094483c3d15b910265e07e92f9e527d017
mpnsk <noreply@example.com>
2023-11-05 15:10:02 +0100

    init

---
+++ src/main/java/com/github/mpnsk/Main.java
@@ -0,0 +1,12 @@
+package com.github.mpnsk;
+
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+        System.out.println("args = " + Arrays.toString(args));
+    }
+}

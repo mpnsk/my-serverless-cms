603160060a4b55de21d253e871dfdc9b35271dd7
mpnsk <noreply@example.com>
2023-12-19 12:55:28 +0100

    copy paste start.spring.io code

---
+++ src/main/java/com/github/mpnsk/myserverlesscms/MyServerlessCmsApplication.java
@@ -0,0 +1,13 @@
+package com.github.mpnsk.myserverlesscms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServerlessCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyServerlessCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mpnsk/myserverlesscms/MyServerlessCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mpnsk.myserverlesscms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServerlessCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
